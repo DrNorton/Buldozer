6c5f7f11e5afe02af4afa1f7e104f788a9f3e308
Andrew <andrey.kozak@altarix.com>
2015-01-21 11:03:02 +0300

    Очередной предрелиз. Добавил рекламу

--- Assets/LevelEventHandler.cs
+++ Assets/LevelEventHandler.cs
@@ -5,9 +5,11 @@ public class LevelEventHandler : MonoBehaviour {
     private SettingsProvider _settingsProvider;
 
     public GameObject SettingsMenu;
+    private WINRTInterfaceHandler _winrtHandler;
     // Use this for initialization
 	void Start () {
         _settingsProvider = (SettingsProvider)(GameObject.Find("SettingsObject").GetComponent("SettingsProvider"));
+        _winrtHandler = (WINRTInterfaceHandler)(GameObject.Find("SettingsObject").GetComponent("WINRTInterfaceHandler"));
 	}
 
     public void SetVolume()
@@ -22,16 +24,35 @@ public class LevelEventHandler : MonoBehaviour {
 
     public void Resume()
     {
-        SettingsMenu.SetActive(false);
+        UnshowMenu();
     }
 	// Update is called once per frame
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            SettingsMenu.SetActive(true);
+            if (!SettingsMenu.activeSelf)
+            {
+                ShowMenu();
+            }
+            else
+            {
+                UnshowMenu();
+            }
         }
 
+    }
+
+    private void UnshowMenu()
+    {
+        _winrtHandler.SendRequest(1, "unshowBanner", requestCallback);
+        SettingsMenu.SetActive(false);
+    }
+
+    private void ShowMenu()
+    {
+        _winrtHandler.SendRequest(1, "showBanner", requestCallback);
+        SettingsMenu.SetActive(true);
     }
 
     public void Replay()
@@ -42,7 +63,13 @@ public class LevelEventHandler : MonoBehaviour {
 
     public void GoToMenu()
     {
+
         _settingsProvider.LoadMenu();
+    }
+
+    private void requestCallback(int mirequestid, string strrequesteddata, string result)
+    {
+        Debug.LogWarning("test");
     }
 
     public void Exit()
---
+++ Assets/Scripts/WINRTInterfaceHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class WINRTInterfaceHandler : MonoBehaviour
+{
+    public delegate void ReceivedCallBack(int miRequestId, string strRequestedData, string result);
+    public ReceivedCallBack OnCallBack;
+    string msPrevRequestedData;
+    int miPrevRequestedId;
+
+    private bool isCreated = false;
+    public event Action<int, string> SendRequestEvent; //Event for handling the call backs
+
+    
+    void Awake()
+    {
+        if (!isCreated)
+        {
+            DontDestroyOnLoad(this.gameObject); //make sure dont delete at all
+            isCreated = true;
+        }
+        else
+            Destroy(this.gameObject);
+    }
+    public bool SendRequest(int requestId, string requestedData, ReceivedCallBack requestCallback)
+    {
+        if (requestCallback != null)
+        {
+            miPrevRequestedId = requestId;
+            msPrevRequestedData = requestedData;
+            OnCallBack = requestCallback;
+        }
+
+        //depends on request id send the request to native code
+        if (SendRequestEvent != null)
+        {
+            SendRequestEvent(requestId, requestedData);
+        }
+
+        return true;
+    }
+
+    public void SendResponse(string msReuestStatus) //which is directly called by the native code
+    {
+        if (OnCallBack == null)
+        {
+            return;
+        } //if no call back required then just return
+
+        else
+        {
+            ReceivedCallBack temp = OnCallBack;
+            OnCallBack = null;
+            temp(miPrevRequestedId, msPrevRequestedData, msReuestStatus);
+        }
+    }
+
+}//Class end
